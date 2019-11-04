a1fe3b25948dcfba49076b8e842dcd73b462391b
RussellPavledakes <56165766+RussellPavledakes@users.noreply.github.com>
2019-11-04 07:54:45 -0800

    made the car move left and right and add the rotate and add somethings to my liking

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // public variables
     public float speed = 20.0f;
+    public float trunspeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // we trun the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * trunspeed * horizontalInput);
     }
 }
